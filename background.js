a95099299df6e18b065bff50bbbc205fa6107bca
Pablo Lopez <devpablolopez@gmail.com>
2021-07-01 18:57:42 -0700

    feat: see list, add to list of topics

--- background.js
+++ background.js
@@ -1,60 +1,99 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({
-        name: "Jack"
+//fetch all topics and store into local storage as soon as background is injected
+var db = null;
+self.importScripts("firebase/app.js", "firebase/realtimedatabase.js");
+firebaseConfig = {
+  /*config data here*/
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function fetchAllTopics() {
+  firebase
+    .database()
+    .ref()
+    .child("sequelize")
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        //set topics into local storage for popup to fetch
+        chrome.storage.local.set({
+          topics: snapshot.val(),
+        });
+      } else {
+        console.log("No data available");
+      }
     });
-});
+}
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        chrome.scripting.insertCSS({
-            target: { tabId: tabId },
-            files: ["./foreground_styles.css"]
-        })
-            .then(() => {
-                console.log("INJECTED THE FOREGROUND STYLES.");
+fetchAllTopics();
 
-                chrome.scripting.executeScript({
-                    target: { tabId: tabId },
-                    files: ["./foreground.js"]
-                })
-                    .then(() => {
-                        console.log("INJECTED THE FOREGROUND SCRIPT.");
-                    });
-            })
-            .catch(err => console.log(err));
-    }
+//set default name when user first loads website
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({
+    name: "randomUser123",
+  });
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'get_name') {
-        chrome.storage.local.get('name', data => {
-            if (chrome.runtime.lastError) {
-                sendResponse({
-                    message: 'fail'
-                });
-
-                return;
-            }
-
-            sendResponse({
-                message: 'success',
-                payload: data.name
-            });
+  if (request.message === "get_name") {
+    chrome.storage.local.get("name", (data) => {
+      if (chrome.runtime.lastError) {
+        sendResponse({
+          message: "fail",
         });
 
-        return true;
-    } else if (request.message === 'change_name') {
-        chrome.storage.local.set({
-            name: request.payload
-        }, () => {
-            if (chrome.runtime.lastError) {
-                sendResponse({ message: 'fail' });
-                return;
-            }
+        return;
+      }
 
-            sendResponse({ message: 'success' });
-        })
+      sendResponse({
+        message: "success",
+        payload: data.name,
+      });
+    });
 
-        return true;
-    }
+    return true;
+  } else if (request.message === "change_name") {
+    chrome.storage.local.set(
+      {
+        name: request.payload,
+      },
+      () => {
+        if (chrome.runtime.lastError) {
+          sendResponse({ message: "fail" });
+          return;
+        }
+
+        sendResponse({ message: "success", payload: request.payload });
+      }
+    );
+
+    return true;
+  } else if (request.message === "get_topics") {
+    chrome.storage.local.get("topics", (data) => {
+      if (chrome.runtime.lastError) {
+        sendResponse({
+          message: "fail",
+        });
+
+        return;
+      }
+
+      sendResponse({
+        message: "success",
+        payload: data,
+      });
+    });
+
+    return true;
+  } else if (request.message === "add_topic") {
+    //add to database
+    firebase
+      .database()
+      .ref()
+      .child("sequelize")
+      .child(request.payload)
+      .set("chrome email here");
+    //call fetch topics
+    fetchAllTopics();
+  }
 });
