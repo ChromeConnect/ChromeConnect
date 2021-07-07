18e6c873840a58ce698ce3abd317309781fb2854
Pablo Lopez <devpablolopez@gmail.com>
2021-07-07 10:10:16 -0700

    feat: alternate between my topics and all topics

--- popup_script.js
+++ popup_script.js
@@ -9,27 +9,90 @@ let searchTopicButton = document.getElementById("searchTopicButton");
 let viewMyTopicsButton = document.getElementById("viewMyTopicsButton");
 
 viewMyTopicsButton.addEventListener("click", function () {
-  chrome.runtime.sendMessage(
-    {
-      message: "get_my_topics",
-    },
-    (response) => {
-      if (response.message === "success") {
-        //console.log(response.payload); //["new", "other"]
-        unorderedList.innerHTML = "";
-        let arrayOfTopics = response.payload;
-        for (let i = 0; i < 10; i++) {
-          if (arrayOfTopics[i]) {
-            let listItemElement = document.createElement("li");
-            listItemElement.appendChild(
-              document.createTextNode(arrayOfTopics[i])
-            );
-            unorderedList.appendChild(listItemElement);
+  if (viewMyTopicsButton.innerText !== "Go Back") {
+    viewMyTopicsButton.innerText = "Go Back";
+    chrome.runtime.sendMessage(
+      {
+        message: "get_my_topics",
+      },
+      (response) => {
+        if (response.message === "success") {
+          //console.log(response.payload); //["new", "other"]
+          unorderedList.innerHTML = "";
+          let arrayOfTopics = response.payload;
+          for (let i = 0; i < 10; i++) {
+            if (arrayOfTopics[i]) {
+              let listItemElement = document.createElement("li");
+              let deleteButton = document.createElement("button");
+              listItemElement.textContent = arrayOfTopics[i];
+              listItemElement.addEventListener("click", function () {
+                chrome.storage.local.get("name", (data) => {
+                  window.open(
+                    //"https://chromechat.herokuapp.com/",
+                    `http://localhost:8080/${data.name}+${replaceSpaces(
+                      this.innerText
+                    )}`,
+                    "popUpWindow",
+                    "height=300,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=no,menubar=yes,location=no,directories=no, status=yes"
+                  );
+                });
+              });
+              deleteButton.textContent = "Delete";
+              deleteButton.addEventListener("click", function () {
+                //somehow send message to delete topic in firebase
+                //close popup
+                window.close();
+                //show alert
+                alert("Successfully Deleted Topic");
+              });
+              listItemElement.appendChild(deleteButton);
+              unorderedList.appendChild(listItemElement);
+            }
           }
         }
       }
-    }
-  );
+    );
+  } else {
+    viewMyTopicsButton.innerText = "View My Topics";
+    unorderedList.innerHTML = "";
+    chrome.runtime.sendMessage(
+      {
+        message: "get_topics",
+      },
+      (response) => {
+        if (response.message === "success") {
+          let topicsObject = response.payload.topics;
+          let arrayOfTopics = Object.keys(topicsObject).reverse();
+          for (let i = 0; i < 10; i++) {
+            if (arrayOfTopics[i]) {
+              let listItemElement = document.createElement("li");
+              listItemElement.addEventListener("click", function () {
+                //get name from local storage
+                chrome.storage.local.get("name", (data) => {
+                  window.open(
+                    //"https://chromechat.herokuapp.com/",
+                    `http://localhost:8080/${data.name}+${replaceSpaces(
+                      this.innerText
+                    )}`,
+                    "popUpWindow",
+                    "height=300,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=no,menubar=yes,location=no,directories=no, status=yes"
+                  );
+                });
+              });
+              if (i === 0) {
+                listItemElement.appendChild(document.createTextNode("Lobby"));
+              } else {
+                listItemElement.appendChild(
+                  document.createTextNode(arrayOfTopics[i])
+                );
+              }
+              unorderedList.appendChild(listItemElement);
+            }
+          }
+        }
+      }
+    );
+  }
 });
 
 searchTopicButton.addEventListener("click", function () {
